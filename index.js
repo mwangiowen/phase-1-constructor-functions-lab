2df55e54940ea120e8d846e6d970b06a384986ef
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-10-23 00:09:16 +0300

    copMPLETD

---
+++ index.js
@@ -0,0 +1,23 @@
+// Define constructor functions for Scooter, Driver, and PickupLocation
+
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+  }
+  
+  function Driver(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
+  
+  function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+  }
+  
+  // Your test code goes here
+  
+  // For example, you can use a testing framework like Mocha and write test cases to ensure that your constructor functions work correctly.
+  
